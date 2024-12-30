f4a791c9287f9f44f107d43a2fe4b510c9486f84
YeChan2266 <dkdlfls9213@naver.com>
2024-12-30 15:09:44 +0900

    Update data2.js

--- data2.js
+++ data2.js
@@ -1,4 +1,4 @@
-let data2 = [
+let data = [
     {
         id: 3,
         img: "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FSBP%252025%2FRMCFLZ0049-Womens-Home-Authentic-Shirt-24-25-White-bellingham-5.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=384&q=50",
@@ -24,4 +24,4 @@ let data2 = [
     }
 ]
 
-export default data2;
+export default data;
